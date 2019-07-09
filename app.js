ee5bb987dedb1ef61a4ec55014e8a5b52a30e83d
Nepcam <cameron.nepe@gmail.com>
2019-07-10 10:48:37 +1200

    set onload function which takes care of the display property

--- app.js
+++ app.js
@@ -9,5 +9,25 @@ function getJokes(e) {
 
   xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      // console.log(response);
+      let output = '';
+
+      if(response.type === 'success') {
+        response.value.forEach(function(joke) {
+          output += `<li>${joke.joke}</li>`
+        })
+      } else {
+        output += '<li>Something went wrong</li>';
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
   e.preventDefault();
 }
